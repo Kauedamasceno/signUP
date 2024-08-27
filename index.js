021c50b02af17b4418c79e037bef765fe89917a4
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-08-27 14:59:45 -0300

    Css error ready

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const submit = document.querySelector('.submit')
 const validate = () => {
   if (!email.value || !email.value.includes('@')){
     errorEmail.textContent = 'Validate email required'
+    email.classList.remove('email');
+    email.classList.add('email_error')
+  }else {
+    errorEmail.textContent = '';
+    email.classList.remove('email_error');
+    email.classList.add("email");
   }
 }
 
 submit.addEventListener('click', (e) => {
-  e.preventDefault()
-  validate()
+  e.preventDefault();
+  validate();
 
 })
