6e395a5144784bc283ffc59bbf0a5fe9485d1a7f
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-08-28 06:55:41 -0300

    HTML and CSS to succes is ready

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const email = document.querySelector(".email")
-const errorEmail = document.querySelector('.error')
+const email = document.querySelector(".email");
+const errorEmail = document.querySelector(".error");
 
-const submit = document.querySelector('.submit')
+const submit = document.querySelector(".submit");
 
 const validate = () => {
-  if (!email.value || !email.value.includes('@')){
-    errorEmail.textContent = 'Validate email required'
-    email.classList.remove('email');
-    email.classList.add('email_error')
-  }else {
-    errorEmail.textContent = '';
-    email.classList.remove('email_error');
+  if (!email.value || !email.value.includes("@")) {
+    errorEmail.textContent = "Validate email required";
+    email.classList.remove("email");
+    email.classList.add("email_error");
+  } else {
+    errorEmail.textContent = "";
+    email.classList.remove("email_error");
     email.classList.add("email");
   }
-}
+};
 
-submit.addEventListener('click', (e) => {
+submit.addEventListener("click", (e) => {
   e.preventDefault();
   validate();
-
-})
+});
